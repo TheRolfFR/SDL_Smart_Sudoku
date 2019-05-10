737e0fe104df1cfd1de2de8aaeae149d4d580acc
TheRolf <yann.lv@live.fr>
2019-05-10 16:58:09 +0200

    addRule + removeRule

---
+++ sudoku_lib/Rules.c
@@ -0,0 +1,55 @@
+//
+// Created by TheRolf on 10/05/2019.
+//
+
+#include "sdl_sudoku.h"
+
+cellList* addRule(cellList *list, cell *cellToAdd) {
+    // on créé l'élément
+    cellList *newHead = malloc(sizeof(cellList));
+    newHead->bloqueur = cellToAdd;
+
+    // si la liste est pas nulle, alors on ajoute en tete
+    if(list != NULL) {
+        newHead->next = list;
+    } else {
+        newHead->next = NULL;
+    }
+
+    // enfin on retourne la nouvelle tête
+    return newHead;
+}
+
+cellList *removeRule(cellList *list, cell *cellToRemove) {
+    // sir ma ilste est vide je renvoie que dalle
+    if(list == NULL) {
+        return NULL;
+    } else {
+        cellList *precedent = NULL;
+        cellList *actuel = list;
+        while(actuel != NULL && actuel->bloqueur != cellToRemove) {
+            precedent = actuel;
+            actuel = actuel->next;
+        }
+
+
+        // s'il l'a trouvé
+        if(actuel != NULL) {
+            // si c'est le premier
+            if(precedent == NULL) {
+                cellList *newHead = actuel->next;
+                free(actuel);
+                return newHead;
+            } else {
+                // si c'est pas le dernier
+                // il faut tous les lier le précédent et le suivant en suprimant l'actuel
+                precedent->next = actuel->next;
+                free(actuel);
+                return list;
+            }
+        } else {
+            // il l'a pas trouve
+            return list;
+        }
+    }
+}
