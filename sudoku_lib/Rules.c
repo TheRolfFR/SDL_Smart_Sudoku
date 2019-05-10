ab2e6a04dbdfbb33501af0ff8b198c613efc6196
TheRolf <yann.lv@live.fr>
2019-05-10 17:31:08 +0200

    print rule

--- sudoku_lib/Rules.c
+++ sudoku_lib/Rules.c
@@ -53,3 +53,12 @@ cellList *removeRule(cellList *list, cell *cellToRemove) {
         }
     }
 }
+
+void printRule(cell* myCell) {
+    printf("1 2 3 4 5 6 7 8 9\n");
+    int i;
+    for(i = 0; i < 9; i++) {
+        printf("%d ", myCell->rules[i]==NULL);
+    }
+    printf("\n");
+}
